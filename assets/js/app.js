eb9a9d683fc39f2afd939e99d9245ad1df6cc675
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-23 22:56:45 +0000

    Refactor files

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,83 @@
 // scroll to top functionality
 const scrollUp = document.querySelector("#scroll-up");
 
+let angleX = 0;
+let angleY = 0;
+let font;
+
+function preload() {
+    font = loadFont("assets/fonts/SpaceGrotesk-VariableFont_wght.ttf"); 
+    loadAsciiFont("assets/fonts/Px437_DOS-V_re_JPN12.ttf");
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+  
+    setAsciiOptions({
+      common: {
+        fontSize: 8,
+      },
+      ascii: {
+        enabled: true,
+        characters: ".:-=+*#%@",
+      },
+      edge: {
+        enabled: true,
+        characters: "-/|\\-/|\\",
+        sobelThreshold: 0.15,
+        sampleThreshold: 16,
+      }
+    });
+}
+
+function draw() {
+    background(0);
+    noStroke();
+    directionalLight(255, 255, 255, 0, 0, -1);
+  
+    let centerX = width / 2;
+    let centerY = height / 2;
+  
+    angleX = map(mouseY, 0, height, PI / 4, -PI / 4);
+    angleY = map(mouseX, 0, width, -PI / 4, PI / 4); 
+  
+    rotateX(angleX);
+    rotateY(angleY);
+  
+    push();
+    translate(0, 0, -100); 
+    fill(0); 
+    noStroke();
+    plane(width, height); 
+    pop();
+  
+    push();
+    translate(0, 0, 0);
+    textFont(font);
+    textSize(300); 
+    textStyle(BOLD); 
+    fill(255);  
+    textAlign(CENTER, CENTER);
+    text("omar", 0, 0); 
+    pop();
+    
+    for (let zOffset = 5; zOffset < 75; zOffset += 5) { 
+      push();
+      translate(0, 0, zOffset); 
+      fill(255, 255, 255, 150); 
+      textFont(font);
+      textSize(300); 
+      textStyle(BOLD); 
+      textAlign(CENTER, CENTER);
+      text("omar", 0, 0);
+      pop();
+    }
+  }
+  
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
+
 scrollUp.addEventListener("click", () => {
     window.scrollTo({
         top: 0,
