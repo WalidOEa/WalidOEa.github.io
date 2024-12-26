ecd0ef15957c1e4961ef0b2e5c44cdf28f4a4aa7
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-26 17:39:51 +0000

    Added chevron

--- assets/js/contactRender.js
+++ assets/js/contactRender.js
@@ -1,5 +1,9 @@
 let linearGradientLeft;
-let linearGradientRight
+let linearGradientRight;
+
+let chevronX, chevronY;
+let chevronWidth = 40;
+let chevronHeight = 40;
 
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
@@ -32,19 +36,63 @@ function setup() {
 
 function draw() {
   background(150, 0, 0);
+
   noStroke();
+  push();
   rotateX(radians(frameCount * 3));
   rotateY(radians(frameCount));
   directionalLight(255, 255, 255, 0, 0, -1);
   torus(200, 50);
+  pop();
 
   linearGradientLeft.angle += 0.35;
   linearGradientRight.angle -= 0.35;
 
-  if (frameCount % 70 == 0) {
+  if (frameCount % 70 === 0) {
     linearGradientLeft.enabled = !linearGradientLeft.enabled;
   }
-  
+
+  chevronX = width / 2 - 100; 
+  chevronY = height / 2 - 100; 
+  push();
+  translate(chevronX, chevronY, 0);
+  fill(0); 
+  beginShape();
+  vertex(0, 0); 
+  vertex(-chevronWidth, chevronHeight / 2);
+  vertex(0, chevronHeight); 
+  endShape(CLOSE);
+  pop();
+}
+
+function mouseMoved() {
+  let mouseXCanvas = mouseX - width / 2;
+  let mouseYCanvas = mouseY - height / 2;
+
+  if (
+    mouseXCanvas > chevronX - chevronWidth &&
+    mouseXCanvas < chevronX &&
+    mouseYCanvas > chevronY &&
+    mouseYCanvas < chevronY + chevronHeight
+  ) {
+    cursor('pointer');
+  } else {
+    cursor('default');
+  }
+}
+
+function mousePressed() {
+  let mouseXCanvas = mouseX - width / 2; 
+  let mouseYCanvas = mouseY - height / 2; 
+
+  if (
+    mouseXCanvas > chevronX - chevronWidth &&
+    mouseXCanvas < chevronX &&
+    mouseYCanvas > chevronY &&
+    mouseYCanvas < chevronY + chevronHeight
+  ) {
+    window.location.href = "index.html";
+  }
 }
 
 function windowResized() {
