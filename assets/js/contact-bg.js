fb2f52aaa3af9effd18b1e8dc2b2c5b83b3afc38
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2024-12-28 20:57:27 +0000

    Refactor file names

--- assets/js/contact-bg.js
+++ assets/js/contact-bg.js
@@ -1,10 +1,6 @@
 let linearGradientLeft;
 let linearGradientRight;
 
-let chevronX, chevronY;
-let chevronWidth = 40;
-let chevronHeight = 40;
-
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
 
@@ -51,47 +47,6 @@ function draw() {
     linearGradientLeft.enabled = !linearGradientLeft.enabled;
   }
 
-  chevronX = width / 2 - 100; 
-  chevronY = height / 2 - 100; 
-  push();
-  translate(chevronX, chevronY, 0);
-  fill(0); 
-  beginShape();
-  vertex(0, 0); 
-  vertex(-chevronWidth, chevronHeight / 2);
-  vertex(0, chevronHeight); 
-  endShape(CLOSE);
-  pop();
-}
-
-function mouseMoved() {
-  let mouseXCanvas = mouseX - width / 2;
-  let mouseYCanvas = mouseY - height / 2;
-
-  if (
-    mouseXCanvas > chevronX - chevronWidth &&
-    mouseXCanvas < chevronX &&
-    mouseYCanvas > chevronY &&
-    mouseYCanvas < chevronY + chevronHeight
-  ) {
-    cursor('pointer');
-  } else {
-    cursor('default');
-  }
-}
-
-function mousePressed() {
-  let mouseXCanvas = mouseX - width / 2; 
-  let mouseYCanvas = mouseY - height / 2; 
-
-  if (
-    mouseXCanvas > chevronX - chevronWidth &&
-    mouseXCanvas < chevronX &&
-    mouseYCanvas > chevronY &&
-    mouseYCanvas < chevronY + chevronHeight
-  ) {
-    window.location.href = "index.html";
-  }
 }
 
 function windowResized() {
