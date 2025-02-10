884a02a4192fd7bfb6b4796434a663e07500dffb
omar-ea <126994619+WalidOEa@users.noreply.github.com>
2025-02-10 21:43:52 +0000

    snake game

--- assets/js/about-bg.js
+++ assets/js/about-bg.js
@@ -1,3 +1,5 @@
+let img;
+
 function preload() {
 }
 
@@ -6,12 +8,26 @@ function setup() {
   canvas.position(0, 0);
   canvas.style('z-index', '-1');
   canvas.style('position', 'absolute');
+
+  setAsciiOptions({
+    common: {
+      fontSize: 4,
+    },
+    ascii: {
+      renderMode: "brightness",
+      characters: " .:-=+*#%@",
+    }
+  });
 }
 
 function draw() {
   background(0);
   noStroke();
 
+  push();
+  translate(-width / 2, -height / 2); 
+  image(img, width / 2 - img.width / 2, height / 2 - img.height / 2);
+  pop();
 }
 
 function windowResized() {
